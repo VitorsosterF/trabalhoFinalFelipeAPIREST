d9bbf184e81a02a651320b584856b1012d7547bf
Caua <caua.crosati@edu.unifil.br>
2025-11-24 18:02:33 -0300

    commit final v2

--- src/main/java/com/VitorsosterF/exercicioPraticoAPIREST/model/Carro.java
+++ src/main/java/com/VitorsosterF/exercicioPraticoAPIREST/model/Carro.java
@@ -8,7 +8,7 @@ public class Carro {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     private String marca;
     private String modelo;
