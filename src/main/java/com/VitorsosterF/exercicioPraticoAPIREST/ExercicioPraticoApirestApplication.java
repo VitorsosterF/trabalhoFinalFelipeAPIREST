a3a01720f92e8c2a309c5bd8a144688110ed606f
VitorsosterF <vitor.soster.f@gmail.com>
2025-11-23 22:17:16 -0300

    commit inicial

---
+++ src/main/java/com/VitorsosterF/exercicioPraticoAPIREST/ExercicioPraticoApirestApplication.java
@@ -0,0 +1,13 @@
+package com.VitorsosterF.exercicioPraticoAPIREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioPraticoApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioPraticoApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/VitorsosterF/exercicioPraticoAPIREST/ExercicioPraticoApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VitorsosterF.exercicioPraticoAPIREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioPraticoApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
