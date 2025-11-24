48aeeb9b0f0409c7dc21caff2670168f9232c6d6
VitorsosterF <vitor.soster.f@gmail.com>
2025-11-23 23:53:37 -0300

    Adição do mvc do Carro (primeiro modelo)

---
+++ src/main/java/com/VitorsosterF/exercicioPraticoAPIREST/controller/CarroController.java
@@ -0,0 +1,54 @@
+package com.VitorsosterF.exercicioPraticoAPIREST.controller;
+
+import com.VitorsosterF.exercicioPraticoAPIREST.model.Carro;
+import com.VitorsosterF.exercicioPraticoAPIREST.repository.CarroRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/carro")
+public class CarroController {
+
+    private final CarroRepository carroRepository;
+
+    public CarroController(CarroRepository carroRepository) {
+        this.carroRepository = carroRepository;
+    }
+
+    @PostMapping
+    public Carro criarCarro(@RequestBody Carro carro) {
+        return carroRepository.save(carro);
+    }
+
+    @GetMapping
+    public List<Carro> listarCarros() {
+        return carroRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Carro buscarPorId(@PathVariable Integer id) {
+        return carroRepository.findById(id).orElse(null);
+    }
+
+    // PUT /api/carro/{id}
+    @PutMapping("/{id}")
+    public Carro atualizarCarro(@PathVariable Integer id, @RequestBody Carro novoCarro) {
+        return carroRepository.findById(id).map(carro -> {
+            carro.setMarca(novoCarro.getMarca());
+            carro.setModelo(novoCarro.getModelo());
+            carro.setAno(novoCarro.getAno());
+            return carroRepository.save(carro);
+        }).orElse(null);
+    }
+
+    // DELETE /api/carro/{id}
+    @DeleteMapping("/{id}")
+    public boolean deletar(@PathVariable Integer id) {
+        if (carroRepository.existsById(id)) {
+            carroRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/VitorsosterF/exercicioPraticoAPIREST/model/Carro.java
@@ -0,0 +1,57 @@
+package com.VitorsosterF.exercicioPraticoAPIREST.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "carros")
+public class Carro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String marca;
+    private String modelo;
+    private Integer ano;
+
+    public Carro() {}
+
+    public Carro(String marca, String modelo, Integer ano)
+    {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+}
